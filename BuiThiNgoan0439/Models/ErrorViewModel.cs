30b158f839f2687688ad593faa6d0053b9a7869c
Buikimngoan240@gmail.com@example.com <Buikimngoan240@gmail.com@example.com>
2023-04-24 15:05:23 +0700

    first commit

---
+++ BuiThiNgoan0439/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BuiThiNgoan0439.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
